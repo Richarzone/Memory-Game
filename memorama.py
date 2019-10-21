4bff1a4d76cc45f35c700647a01f2035274efb40
Glazzerino <fztmon@gmail.com>
2019-10-20 19:06:56 -0700

    betterinput

--- memorama.py
+++ memorama.py
@@ -1,5 +1,12 @@
 from random import shuffle
-
+def betterinput (string):
+    condition = False
+    while condition:
+        try:
+            int(string)
+            condition = True
+        except:
+            print("Por favor ingrese un numero valido")
 def crearListanum():
     lista = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9','10','10','11','11','12','12','13','13','14','14','15','15','16','16','17','17','18','18']
     shuffle(lista)
